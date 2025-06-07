3c9a4e22a5771a44dc07fcc34f01ddbe21304cee
gedalyas <davialmeida.souto16@gmail.com>
2025-06-06 23:54:22 -0300

    foto alterada com algumas funcionalidades de movimento

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,13 +1,37 @@
 import React from 'react';
+import { FaInstagram, FaLinkedin, FaEnvelope, FaWhatsapp, FaGithub } from 'react-icons/fa';
+import FotoSetup from'../assets/images/FotoSetup.jpg';
+import "../design/contact.css";
 import Menu from './Menu';
 
 function Contact() {
   return (
-    <div>
-      <h2>Contact</h2>
-      <p>Me encontre nas redes sociais ou envie uma mensagem...</p>
-      <div>
-        <Menu/>
+    <div className="contact-container">
+      <Menu />
+      <div className="contact-left">
+        <h2><strong>Get in Touch</strong></h2>
+        <p>Let’s connect! Find me on the platforms below:</p>
+        <div className="contact-icons">
+          <a href="https://www.instagram.com/davisouto.a?igsh=MWttZzdqZjBocXRwdw==" target="_blank" rel="noopener noreferrer" className="icon-link">
+            <FaInstagram />
+          </a>
+          <a href="https://www.linkedin.com/in/davi-alsouto" target="_blank" rel="noopener noreferrer" className="icon-link">
+            <FaLinkedin />
+          </a>
+          <a href="mailto:davialsouto@gmail.com" className="icon-link">
+            <FaEnvelope />
+          </a>
+          <a href="https://wa.me/5531993112726?text=Ol%C3%A1%2C%20gostaria%20de%20saber%20mais%20sobre%20os%20seus%20serviços" target="_blank" rel="noopener noreferrer" className="icon-link">
+            <FaWhatsapp />
+          </a>
+          <a href="https://github.com/gedalyas" target="_blank" rel="noopener noreferrer" className="icon-link">
+            <FaGithub />
+          </a>
+        </div>
+      </div>
+
+      <div className="contact-right">
+        <img src={FotoSetup} alt="My Setup" className="setup-image" />
       </div>
     </div>
   );
